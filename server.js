32807b07dd52014106c19fecbd5aeec415b4e3c2
mpayret <mpayret@gmail.com>
2025-07-12 10:44:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // usa el puerto que Railway provee
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando en http://localhost:${PORT}`);
+});
+const express = require("express");
 const path = require("path");
 
 const app = express();
